7da782ba9754a38c45b2b2fbc91dee7f8df17c5a
windylung <jisoolee031@gmail.com>
2024-06-06 10:42:23 +0900

    fix. uri

--- lib/book_list.dart
+++ lib/book_list.dart
@@ -20,7 +20,7 @@ class _BookListState extends State<BookList> {
 
   Future<List<Book>> fetchBooks() async {
     final response =
-    await http.get(Uri.parse('http://localhost:8080/book/list'));
+    await http.get(Uri.parse('http://humanbook.kr/book/list'));
 
     if (response.statusCode == 200) {
       List<dynamic> booksJson = jsonDecode(response.body);
