93c73c816b5d288a734ccd1d0a00c2f4addb11ef
windylung <jisoolee031@gmail.com>
2024-06-10 23:46:50 +0900

    feat. logout

--- lib/header.dart
+++ lib/header.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_java_humanbook/auth_provider.dart';
@@ -13,6 +14,31 @@ class CustomHeader extends StatefulWidget implements PreferredSizeWidget {
 }
 
 class _CustomHeaderState extends State<CustomHeader> {
+
+
+  Future<void> _handleLogout() async {
+    try {
+      Dio dio = Dio();
+      Response response = await dio.post(
+        'http://humanbook.kr/api/logout',
+      );
+
+      if (response.statusCode == 200) {
+        // logout 성공 처리
+        print('logout 성공');
+        Provider.of<AuthProvider>(context, listen: false).logout(); // 로컬 로그아웃 처리
+        Navigator.pushNamed(context, '/'); // 로그아웃 후 '/'로 이동
+      } else {
+        // logout 실패 처리
+        print('logout 실패');
+      }
+    } catch (e) {
+      // 네트워크 오류 등 예외 처리
+      print('오류: $e');
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AuthProvider>(
@@ -26,32 +52,15 @@ class _CustomHeaderState extends State<CustomHeader> {
           title: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Container(
-                width: 120, // 'MY PAGE' 버튼의 가로 길이 설정
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/mypage'); // '/mypage'로 이동하도록 수정
-                  },
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 8.0), // 패딩 설정
-                    minimumSize: Size(120, 40), // 최소 크기 설정
-                  ),
-                  child: Text(
-                    'MY PAGE',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16, // 글꼴 크기 조정
-                    ),
-                    overflow: TextOverflow.visible, // 텍스트가 넘칠 경우 생략하지 않음
-                    textAlign: TextAlign.center, // 텍스트 가운데 정렬
-                  ),
-                ),
-              ),
               Center(
-                child: Image.asset(
-                  'assets/logo.png', // 이미지 경로 설정
-                  height: 50,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/'); // 로고 클릭 시 '/'로 이동
+                  },
+                  child: Image.asset(
+                    'assets/logo.png', // 이미지 경로 설정
+                    height: 50,
+                  ),
                 ),
               ),
               Row(
@@ -104,55 +113,79 @@ class _CustomHeaderState extends State<CustomHeader> {
                       ),
                     ),
                   ),
-                  Center(
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/join'); // '/join'로 이동하도록 수정
-                      },
-                      child: Text(
-                        'JOIN',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        overflow: TextOverflow.visible, // 텍스트가 사라짐 없이 보이도록 설정
-                        textAlign: TextAlign.center, // 텍스트 가운데 정렬
-                      ),
-                    ),
-                  ),
-                  if (!isLoggedIn)
-                    Center(
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/login_new'); // '/login'으로 이동하도록 수정
-                        },
-                        child: Text(
-                          'LOGIN',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
+                  if (isLoggedIn)
+                    Row(
+                      children: [
+                        Center(
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/mypage'); // '/mypage'로 이동하도록 수정
+                            },
+                            child: Text(
+                              'MY PAGE',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              overflow: TextOverflow.visible, // 텍스트가 사라짐 없이 보이도록 설정
+                              textAlign: TextAlign.center, // 텍스트 가운데 정렬
+                            ),
                           ),
-                          overflow: TextOverflow.visible, // 텍스트가 사라짐 없이 보이도록 설정
-                          textAlign: TextAlign.center, // 텍스트 가운데 정렬
                         ),
-                      ),
+                        Center(
+                          child: TextButton(
+                            onPressed: () {
+                              _handleLogout();
+                            },
+                            child: Text(
+                              'LOGOUT',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              overflow: TextOverflow.visible, // 텍스트가 사라짐 없이 보이도록 설정
+                              textAlign: TextAlign.center, // 텍스트 가운데 정렬
+                            ),
+                          ),
+                        ),
+                      ],
                     )
                   else
-                    Center(
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/mypage'); // '/mypage'로 이동하도록 수정
-                        },
-                        child: Text(
-                          'MY PAGE',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
+                    Row(
+                      children: [
+                        Center(
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/login_new'); // '/login'으로 이동하도록 수정
+                            },
+                            child: Text(
+                              'LOGIN',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              overflow: TextOverflow.visible, // 텍스트가 사라짐 없이 보이도록 설정
+                              textAlign: TextAlign.center, // 텍스트 가운데 정렬
+                            ),
                           ),
-                          overflow: TextOverflow.visible, // 텍스트가 사라짐 없이 보이도록 설정
-                          textAlign: TextAlign.center, // 텍스트 가운데 정렬
                         ),
-                      ),
+                        Center(
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/join'); // '/join'으로 이동하도록 수정
+                            },
+                            child: Text(
+                              'JOIN',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              overflow: TextOverflow.visible, // 텍스트가 사라짐 없이 보이도록 설정
+                              textAlign: TextAlign.center, // 텍스트 가운데 정렬
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                 ],
               ),
