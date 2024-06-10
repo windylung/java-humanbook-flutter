1935a278d0dadddb56156429302269444edfbda5
windylung <jisoolee031@gmail.com>
2024-06-10 23:28:18 +0900

    fix. join api and login

--- lib/join.dart
+++ lib/join.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_java_humanbook/color.dart';
 import 'package:flutter_java_humanbook/joinRequest.dart';
-import 'package:flutter_java_humanbook/joinRequest.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_java_humanbook/auth_provider.dart';
 
 class JoinScreen extends StatefulWidget {
   @override
@@ -13,6 +15,44 @@ class _JoinScreenState extends State<JoinScreen> {
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _passwordCheckController = TextEditingController();
   final TextEditingController _nameController = TextEditingController();
+
+  void _showDialog(String title, String content) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: Colors.white, // 배경색 흰색
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        content: Text(
+          content,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          Center(
+            child: TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text(
+                '확인',
+                style: TextStyle(
+                  color: COLOR_BLUE,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   Future<void> _handleJoin() async {
     String id = _idController.text;
@@ -39,6 +79,8 @@ class _JoinScreenState extends State<JoinScreen> {
       return;
     }
 
+
+
     try {
       Dio dio = Dio();
 
@@ -59,13 +101,29 @@ class _JoinScreenState extends State<JoinScreen> {
       if (response.statusCode == 200) {
         // 회원가입 성공 처리
         print('회원가입 성공');
+        _showDialog('알림', '회원가입에 성공했습니다.');
+
+        // 자동 로그인 시도
+        _handleLogin(id, password);
       } else {
         // 회원가입 실패 처리
         print('회원가입 실패');
+        _showDialog('오류', '회원가입에 실패했습니다.');
       }
     } catch (e) {
       // 네트워크 오류 등 예외 처리
       print('오류: $e');
+      _showDialog('오류', '회원가입 중 오류가 발생했습니다.');
+    }
+  }
+
+  Future<void> _handleLogin(String id, String password) async {
+    try {
+      await Provider.of<AuthProvider>(context, listen: false).login(id, password);
+      Navigator.pushReplacementNamed(context, '/'); // 메인 화면으로 이동
+    } catch (e) {
+      print('로그인 오류: $e');
+      _showDialog('오류', '자동 로그인에 실패했습니다.');
     }
   }
 
