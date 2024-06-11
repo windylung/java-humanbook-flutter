d0d90ae5ef1c86877473d97672d1f3b70153a26c
Lim <jameslim03@dankook.ac.kr>
2024-06-12 08:58:16 +0900

    11th commit

--- lib/auth_provider.dart
+++ lib/auth_provider.dart
@@ -1,3 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+
 class AuthProvider with ChangeNotifier {
   bool _isLoggedIn = false;
   String _cookie = '';
