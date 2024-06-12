1c461821ee9334b7e631515eb4f50fcce2f34cf2
im-minji <mjlim514@gmail.com>
2024-06-12 11:02:30 +0900

    loginerror

--- lib/login_new.dart
+++ lib/login_new.dart
@@ -12,44 +12,6 @@ class _LoginScreenState extends State<LoginScreenNew> {
   final TextEditingController _passwordController = TextEditingController();
   String _responseMessage = '';
 
-    void _showDialog(String title, String content) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        backgroundColor: Colors.white, // 배경색 흰색
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        content: Text(
-          content,
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.black,
-          ),
-        ),
-        actions: [
-          Center(
-            child: TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: Text(
-                '확인',
-                style: TextStyle(
-                  color: COLOR_BLUE,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   Future<void> _login() async {
     try {
       await Provider.of<AuthProvider>(context, listen: false).login(
@@ -58,14 +20,11 @@ class _LoginScreenState extends State<LoginScreenNew> {
       );
       Navigator.pushReplacementNamed(context, '/');
     } catch (e) {
-      _showDialog('오류', '로그인에 실패했습니다.');
       setState(() {
         _responseMessage = 'Error: $e';
       });
     }
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -111,3 +70,4 @@ class _LoginScreenState extends State<LoginScreenNew> {
     );
   }
 }
+
